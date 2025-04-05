0916ac62bacd65b349d494797de7c4b1d28d9051
Mikael Burgos <51490107+mikaelburgos97@users.noreply.github.com>
2025-04-05 15:20:02 -0400

    Configuración inicial del proyecto con Express, SQLite y React

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Gestor de Tareas</h1>
+        <p>Una aplicación CRUD simple con React y SQLite</p>
+      </header>
+      <main className="App-main">
+        <p>Configuración inicial del proyecto</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
